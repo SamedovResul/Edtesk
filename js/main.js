b1af0d31a8f08078dbf3b1a796046d664716a810
SamedovRasul <samedovrasul7@gmail.com>
2022-05-08 22:35:58 +0400

    new changes

--- js/main.js
+++ js/main.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
     responsive: [{
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
