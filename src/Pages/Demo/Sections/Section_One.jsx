14bed1879d0e50989f77ea5a6c267411b8bae581
SamedovResul <samedovrasul7@gmail.com>
2024-01-03 20:45:28 +0400

    .

--- src/Pages/Demo/Sections/Section_One.jsx
+++ src/Pages/Demo/Sections/Section_One.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Card1 from "../../../assets/first.png"
-import Card2 from "../../../assets/second.png"
-import Card3 from "../../../assets/third.png"
-import Card4 from "../../../assets/fourth.png"
-import Card5 from "../../../assets/fiveth.png"
-import Card6 from "../../../assets/sixth.png"
+import img1 from '../../../assets/image0.png'
+import img2 from '../../../assets/image1.png'
+import img3 from '../../../assets/image2.png'
 
 const SectionOne = () => {
     const cardsData=[
-        {id:1,text:"Enteskedu",content:"Digital Agency",img:Card1},
-        {id:2,text:"Enteskedu",content:"Digital Agency",img:Card1},
-        {id:3,text:"Enteskedu",content:"Digital Agency",img:Card1},
-        {id:4,text:"Enteskedu",content:"Digital Agency",img:Card1},
-        {id:5,text:"Enteskedu",content:"Digital Agency",img:Card1},
-        {id:6,text:"Enteskedu",content:"Digital Agency",img:Card1},
+        {id:1,text:"Enteskedu",content:"Digital Agency",img:img1,link:"https://www.instagram.com/p/C1em6UiIjTg/?igsh=MXVsaTJodW1hOXAyZA=="},
+        {id:2,text:"Enteskedu",content:"Digital Agency",img:img2,link:"https://www.instagram.com/p/C1Z5ov_I9v7/?igsh=MTFtdG41N3BrZTY2bA=="},
+        {id:3,text:"Enteskedu",content:"Digital Agency",img:img3,link:"https://www.instagram.com/p/C1BtlWRALxw/?igsh=MWJ3aTUybXlyNnU4NA=="},
+        // {id:4,text:"Enteskedu",content:"Digital Agency",img:Card1,link:"https://www.instagram.com/p/C0yYRbagANI/?igsh=dDN1Nm05dXJobmJ0"},
+        // {id:5,text:"Enteskedu",content:"Digital Agency",img:Card1,link:"https://www.instagram.com/p/C1Z5ov_I9v7/?igsh=MTFtdG41N3BrZTY2bA=="},
+        // {id:6,text:"Enteskedu",content:"Digital Agency",img:Card1,link:"https://www.instagram.com/p/C1BtlWRALxw/?igsh=MWJ3aTUybXlyNnU4NA=="},
     ]
   return (
     <div className="section-one">
@@ -24,7 +22,9 @@ const SectionOne = () => {
         <div className="demo-cards">
             {cardsData.map(card=>(
                 <div key={card.id} className="card-container">
+                  <a  href={card.link}  >
                     <img src={card.img} alt="/" />
+                  </a>
                     <p>{card.text}</p>
                     <span>{card.content}</span>
                 </div>
--- src/Pages/Home/Sections/Section_One.jsx
+++ src/Pages/Home/Sections/Section_One.jsx
@@ -6,7 +6,7 @@ const SectionOne = () => {
     <div className='section-one'>
         <div className="container">
             <div className="content">
-              <h1> <span> IT and Web solutions </span><br /> for your business </h1>
+              <h1>  <span> IT and Web solutions  </span><br /> for your business </h1>
               <p>We create websites with complex logic, mobile applications, personal accounts and integrate with other systems. We create automation systems within the business...</p>
               <p> contact@enteskedu.com </p>
             </div>
