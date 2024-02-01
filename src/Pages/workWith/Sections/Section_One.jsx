2a994aecc252bfd5f243dbd99784101e4f0aa5b3
SamedovResul <samedovrasul7@gmail.com>
2024-02-01 10:17:13 +0400

    .

--- src/Pages/workWith/Sections/Section_One.jsx
+++ src/Pages/workWith/Sections/Section_One.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "../WorkWith.css";
+import img1 from '../../../assets/partnership/img1.jpg'
+import img2 from '../../../assets/partnership/img2.jpg'
+import img3 from '../../../assets/partnership/img3.jpg'
+import img4 from '../../../assets/partnership/img4.webp'
+import img5 from '../../../assets/partnership/img5.jpg'
+import img6 from '../../../assets/partnership/img6.png'
+import img7 from '../../../assets/partnership/img7.gif'
+import img8 from '../../../assets/partnership/img8.jpg'
 import SupportImg1 from "../../../assets/anowar.jpg"
 const SectionOne = () => {
     const supportData=[
-        {id:1, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:2, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:3, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:4, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:1, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:2, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:3, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"},
-        {id:4, logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeymOOX6DKuOJeN9zmZ4b9vNZapUyPkcWp2xyXabxZYA&s"}
+        {id:1, logo:img1},
+        {id:2, logo:img2},
+        {id:3, logo:img3},
+        {id:4, logo:img4},
+        {id:1, logo:img5},
+        {id:2, logo:img6},
+        {id:3, logo:img7},
+        {id:4, logo:img8}
     ]
   return (
     <div className="section-one">
